59bb8c0c6be23ca74c9423f1e2deef199b538ef2
Paul Elliott <paul.elliott@kitware.com>
2024-01-24 15:29:52 -0500

    feat(App): add sidebar layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "./App.css";
+
+const SIDE_BAR_START_WIDTH = 200; // in pixels
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [width, setWidth] = React.useState(SIDE_BAR_START_WIDTH);
+
+  const handlePointerDown = (e: React.PointerEvent) => {
+    const start = e.clientX;
+    const startWidth = width;
+    document.body.style.cursor = "ew-resize";
+
+    const handleMove = (e: PointerEvent) => {
+      const delta = e.clientX - start;
+      setWidth(startWidth + delta);
+    };
+
+    const handlePointerUp = () => {
+      document.removeEventListener("pointermove", handleMove);
+      document.removeEventListener("pointerup", handlePointerUp);
+      document.body.style.removeProperty("cursor");
+    };
+
+    document.addEventListener("pointermove", handleMove);
+    document.addEventListener("pointerup", handlePointerUp);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex w-full h-full">
+      <div className="relative bg-slate-50">
+        <div
+          className="bg-slate-50 overflow-hidden dark:bg-slate-800/25"
+          style={{ width: `${width}px` }}
+        >
+          Materials
+        </div>
+
+        <div
+          className="pointer-events-auto absolute top-1/2 -right-10 w-8 cursor-ew-resize"
+          draggable="false"
+          style={{
+            userSelect: "none",
+            touchAction: "pan-y",
+          }}
+          onPointerDown={handlePointerDown}
+        >
+          <div className="w-1.5 h-8 bg-slate-500/60 border-4 rounded-full"></div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div className="flex-1 flex place-content-center">
+        <div>Material</div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
